96d9b2866e6af6fcb7b8caa378e2270b993af451
Al Ameen S <alameensameen77@gmail.com>
2026-08-04 14:59:44 +0530

    feat : added bullmq package

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,39 @@
 import app from "./app.js";
-import redis from "./config/redis.js";
 import {
-	connectDatabase,
-	disconnectDatabase,
+  connectDatabase,
+  disconnectDatabase,
 } from "./infrastructure/database/db.js";
 import { connectRedis, disconnectRedis } from "./infrastructure/redis/redis.js";
 import { PORT } from "./shared/constants/app.constants.js";
 
 async function bootstrap() {
-	try {
-		await connectDatabase();
+  try {
+    await connectDatabase();
 
     await connectRedis()
 
-		const server = app.listen(PORT, () => {
-			console.log(`🚀 Server running on port ${PORT}`);
-		});
+    const server = app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
 
-		async function shutdown(signal: string) {
-			console.log(`\n${signal} received. Shutting down...`);
+    async function shutdown(signal: string) {
+      console.log(`\n${signal} received. Shutting down...`);
 
-			server.close(async () => {
-				await disconnectRedis()
-				await disconnectDatabase();
+      server.close(async () => {
+        await disconnectRedis()
+        await disconnectDatabase();
 
-				console.log("✅ Shutdown completed");
-				process.exit(0);
-			});
-		}
+        console.log("✅ Shutdown completed");
+        process.exit(0);
+      });
+    }
 
-		process.on("SIGINT", () => shutdown("SIGINT"));
-		process.on("SIGTERM", () => shutdown("SIGTERM"));
-	} catch (error) {
-		console.error("Failed to start application", error);
-		process.exit(1);
-	}
+    process.on("SIGINT", () => shutdown("SIGINT"));
+    process.on("SIGTERM", () => shutdown("SIGTERM"));
+  } catch (error) {
+    console.error("Failed to start application", error);
+    process.exit(1);
+  }
 }
 
 bootstrap();
