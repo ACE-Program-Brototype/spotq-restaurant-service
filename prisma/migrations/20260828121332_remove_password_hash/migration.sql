c4e51e7d79d0257fe49ca3159b94ed538a795c34
Surynx <sooryanarayanan1082004@gmail.com>
2026-08-30 22:10:12 +0530

    fix: updated the prisma client

--- prisma/migrations/20260828121332_remove_password_hash/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `passwordHash` on the `restaurants` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "restaurants" DROP COLUMN "passwordHash";
