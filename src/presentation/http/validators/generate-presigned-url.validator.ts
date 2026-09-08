4a2d0c445bce7d85c4e3a1cd920d87490a72e4f6
Surynx <sooryanarayanan1082004@gmail.com>
2026-09-08 16:58:23 +0530

    refactor: changed the entity_id to onboarding_id

--- src/presentation/http/validators/generate-presigned-url.validator.ts
+++ src/presentation/http/validators/generate-presigned-url.validator.ts
@@ -2,7 +2,7 @@ import { FileCategory } from "@shared/storage/file-category.enum";
 import { z } from "zod";
 
 export const generatePresignedUrlSchema = z.object({
-	entity_id: z.uuid(),
+	onboarding_id: z.uuid(),
 	file_name: z.string().min(1).max(255),
 	content_type: z.string().min(1),
 	file_category: z.enum(FileCategory),
