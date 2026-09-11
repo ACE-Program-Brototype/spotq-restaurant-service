7c729aee481d6a538c32c374457f3a636df7e755
Surynx <sooryanarayanan1082004@gmail.com>
2026-09-11 16:49:53 +0530

    test(use-cases): add completeOnboarding mock to validate-invitation test

--- tests/unit/application/use-cases/validate-invitation.use-case.spec.ts
+++ tests/unit/application/use-cases/validate-invitation.use-case.spec.ts
@@ -46,6 +46,7 @@ describe("ValidateInvitationUseCase", () => {
 			create: jest.fn(),
 			createRestaurant: jest.fn(),
 			update: jest.fn(),
+			completeOnboarding: jest.fn(),
 		};
 
 		invitationTokenService = {
