bfb98be9cccda0b9a64f0ffee317f17ff8200a5e
Surynx <sooryanarayanan1082004@gmail.com>
2026-09-17 16:51:48 +0530

    fix: updated the route of storage

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import systemRouter from "@/presentation/http/routes/system.routes";
 import { APP_ENV, APP_NAME } from "@/shared/constants/app.constants.ts";
 import { HTTP_STATUS } from "@/shared/constants/http.constants.ts";
 import { messages } from "@/shared/constants/message.constants.ts";
-import { STAFF_ROUTES } from "@/shared/constants/route.constants.ts";
+import { STAFF_ROUTES, STORAGE_ROUTES } from "@/shared/constants/route.constants.ts";
 import { sendSuccessResponse } from "@/shared/response/api-response.ts";
 import { restaurantRouter } from "./presentation/http/routes/restaurant.routes";
 
@@ -44,7 +44,7 @@ app.get("/", (_req, res) => {
 app.use(STAFF_ROUTES.BASE, staffRouter);
 app.use("/", systemRouter);
 app.use("/", restaurantRouter);
-app.use("/", storageRouter);
+app.use(STORAGE_ROUTES.BASE, storageRouter);
 
 // 404 & Error handlers
 app.use(notFoundHandler);
