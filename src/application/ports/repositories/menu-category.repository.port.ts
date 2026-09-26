c5bd796424c65ed3df5f7309944443ba1a10947a
Ajex Joshy <ajexjoshywork@gmail.com>
2026-09-26 21:50:48 +0530

    refactor(menu): align menu category repository port and remove orphaned list use case [SCRUM-898]

--- src/application/ports/repositories/menu-category.repository.port.ts
+++ src/application/ports/repositories/menu-category.repository.port.ts
@@ -1,12 +1,3 @@
-import type { MenuCategory } from "@/domain/entities/menu-category.entity.ts";
+import type { IMenuCategoryRepository } from "@/domain/repositories/menu-category.repository.interface.ts";
 
-export interface IMenuCategoryRepositoryPort {
-	findById(id: string): Promise<MenuCategory | null>;
-	findByRestaurantId(restaurantId: string): Promise<MenuCategory[]>;
-	findByNameAndRestaurantId(
-		restaurantId: string,
-		name: string,
-	): Promise<MenuCategory | null>;
-	getNextDisplayOrder(restaurantId: string): Promise<number>;
-	create(category: MenuCategory): Promise<MenuCategory>;
-}
+export type { IMenuCategoryRepository };
--- src/application/ports/use-cases/list-menu-categories.use-case.port.ts
+++
@@ -1,5 +0,0 @@
-import type { MenuCategoryResponseDto } from "@/application/dtos/menu/create-menu-category.dto.ts";
-
-export interface IListMenuCategoriesUseCase {
-	execute(restaurantId: string): Promise<MenuCategoryResponseDto[]>;
-}
